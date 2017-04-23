65050fd726b8d78caf4e4827c5ed7c7cf527af47
Michael Sulit <michael.sulit@gmail.com>
2017-04-23 10:15:40 -0400

    Add files via upload

---
+++ run.py
@@ -0,0 +1,87 @@
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse, Message
+from twilio.twiml.voice_response import VoiceResponse
+from twilio.rest import Client
+
+app = Flask(__name__)
+last_url = []
+PhoneNumbers=[]
+
+account_sid = "AC63f63bab49ffb5b3df2dd3866c38e1b2"
+auth_token = "0bf99c196c9c2f8c6568d0c1948cb65a"
+client = Client(account_sid, auth_token)
+
+
+
+@app.route("/text", methods=['GET', 'POST'])
+def text_receive():
+    """Respond to incoming calls with a simple text message."""
+
+    from_number = request.values.get('From', None)
+    text_body = request.values.get('Body', None)
+    if(from_number == "+19739074375" and text_body == "go"):
+        begin_calls()
+    if from_number:
+        PhoneNumbers.append(from_number)
+    print PhoneNumbers
+    resp = MessagingResponse().message(from_number)
+    return str(resp)
+
+@app.route("/next-call", methods=['GET', 'POST'])
+def next_call():
+    resp = VoiceResponse()
+    PhoneNumbers.pop();
+    print "--"
+    print last_url
+    print "--"
+    if not last_url:
+        resp.say("Welcome to today's game of telephone. At the beep, record a phrase to get passed around.")
+    else:
+        resp.say("Welcome to today's game of telephone. At the beep, try to repeat the following phrase as best as you can.")
+        resp.play(last_url.pop())
+
+    resp.record(maxLength="5", action="/handle-recording")
+    return str(resp)
+
+@app.route("/last-call", methods=['GET', 'POST'])
+def last_call():
+    resp = VoiceResponse()
+    resp.say("Thanks for playing telephone. Here is the final phrase that resulted.")
+    resp.play(last_url.pop())
+    return str(resp)
+
+@app.route("/handle-recording", methods=['GET', 'POST'])
+def handle_recording():
+    """Play back the caller's recording."""
+
+    #recording_url = request.values.get("RecordingUrl", None)
+    resp = VoiceResponse()
+
+
+    last_url.append(request.values.get("RecordingUrl", None))
+    if PhoneNumbers:
+        call = client.api.account.calls.create(to=PhoneNumbers.peek(),  # Any phone number
+            from_="+12013971017", # Must be a valid Twilio number
+            url="http://f81a966a.ngrok.io/next-call")
+    else:
+        call = client.api.account.calls.create(to=PhoneNumbers.peek(),  # Any phone number
+            from_="+12013971017", # Must be a valid Twilio number
+            url="http://f81a966a.ngrok.io/last-call")
+
+
+    resp.say("Goodbye.")
+    print last_url
+    return str(resp)
+
+def begin_calls():
+
+    PhoneNumbers.reverse()
+
+    if PhoneNumbers:
+        call = client.api.account.calls.create(to=PhoneNumbers.peek(),  # Any phone number
+            from_="+12013971017", # Must be a valid Twilio number
+            url="http://f81a966a.ngrok.io/next-call")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
